8c7ac37a80a270b0696d3fcaed54fe9da4d52216
Simon Wacker <simon.wacker@ise.faunhofer.de>
2025-05-30 18:07:22 +0200

    Add required property `Exists` to `Publication` to identify whether the entity exists

---
+++ backend/src/Migrations/migrate_from_20250530151605_ModelReferenceProperly_to_20250530155318_AddRequiredPropertyExistsToPublication.sql
@@ -0,0 +1,16 @@
+﻿START TRANSACTION;
+ALTER TABLE database."photovoltaic_data_Approvals" ADD "Statement_Publication_Exists" boolean;
+
+ALTER TABLE database."optical_data_Approvals" ADD "Statement_Publication_Exists" boolean;
+
+ALTER TABLE database."hygrothermal_data_Approvals" ADD "Statement_Publication_Exists" boolean;
+
+ALTER TABLE database."geometric_data_Approvals" ADD "Statement_Publication_Exists" boolean;
+
+ALTER TABLE database."calorimetric_data_Approvals" ADD "Statement_Publication_Exists" boolean;
+
+INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20250530155318_AddRequiredPropertyExistsToPublication', '9.0.5');
+
+COMMIT;
+
---
+++ backend/src/Migrations/rollback_from_20250530151605_ModelReferenceProperly_to_20250530155318_AddRequiredPropertyExistsToPublication.sql
@@ -0,0 +1,16 @@
+﻿START TRANSACTION;
+ALTER TABLE database."photovoltaic_data_Approvals" DROP COLUMN "Statement_Publication_Exists";
+
+ALTER TABLE database."optical_data_Approvals" DROP COLUMN "Statement_Publication_Exists";
+
+ALTER TABLE database."hygrothermal_data_Approvals" DROP COLUMN "Statement_Publication_Exists";
+
+ALTER TABLE database."geometric_data_Approvals" DROP COLUMN "Statement_Publication_Exists";
+
+ALTER TABLE database."calorimetric_data_Approvals" DROP COLUMN "Statement_Publication_Exists";
+
+DELETE FROM "__EFMigrationsHistory"
+WHERE "MigrationId" = '20250530155318_AddRequiredPropertyExistsToPublication';
+
+COMMIT;
+

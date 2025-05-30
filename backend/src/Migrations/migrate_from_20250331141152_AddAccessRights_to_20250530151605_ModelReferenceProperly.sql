ca58dd4d12541544dc042c9b205245c6fb4dc64e
Simon Wacker <simon.wacker@ise.faunhofer.de>
2025-05-30 17:23:20 +0200

    Add SQL migration and rollback scripts

---
+++ backend/src/Migrations/migrate_from_20250331141152_AddAccessRights_to_20250530151605_ModelReferenceProperly.sql
@@ -0,0 +1,228 @@
+﻿START TRANSACTION;
+ALTER TABLE database.photovoltaic_data ADD "DataAccessRights_AllowedApplications" text[];
+
+ALTER TABLE database.photovoltaic_data ADD "DataAccessRights_AllowedInstitutions" uuid[];
+
+ALTER TABLE database.photovoltaic_data ADD "DataAccessRights_AllowedUserAndQuantity" jsonb;
+
+ALTER TABLE database.optical_data ADD "DataAccessRights_AllowedApplications" text[];
+
+ALTER TABLE database.optical_data ADD "DataAccessRights_AllowedInstitutions" uuid[];
+
+ALTER TABLE database.optical_data ADD "DataAccessRights_AllowedUserAndQuantity" jsonb;
+
+ALTER TABLE database.hygrothermal_data ADD "DataAccessRights_AllowedApplications" text[];
+
+ALTER TABLE database.hygrothermal_data ADD "DataAccessRights_AllowedInstitutions" uuid[];
+
+ALTER TABLE database.hygrothermal_data ADD "DataAccessRights_AllowedUserAndQuantity" jsonb;
+
+ALTER TABLE database.geometric_data ADD "DataAccessRights_AllowedApplications" text[];
+
+ALTER TABLE database.geometric_data ADD "DataAccessRights_AllowedInstitutions" uuid[];
+
+ALTER TABLE database.geometric_data ADD "DataAccessRights_AllowedUserAndQuantity" jsonb;
+
+ALTER TABLE database.calorimetric_data ADD "DataAccessRights_AllowedApplications" text[];
+
+ALTER TABLE database.calorimetric_data ADD "DataAccessRights_AllowedInstitutions" uuid[];
+
+ALTER TABLE database.calorimetric_data ADD "DataAccessRights_AllowedUserAndQuantity" jsonb;
+
+INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20250527144412_AddDataAccessRights', '9.0.5');
+
+ALTER TABLE database.access_rights DROP CONSTRAINT "PK_access_rights";
+
+ALTER TABLE database.access_rights RENAME TO institution_access_rights;
+
+ALTER TABLE database.institution_access_rights ADD CONSTRAINT "PK_institution_access_rights" PRIMARY KEY ("Id");
+
+INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20250527150325_MoveInstitutionAccessRights', '9.0.5');
+
+ALTER TABLE database."photovoltaic_data_Approvals" RENAME COLUMN "Standard_Year" TO "Statement_Standard_Year";
+
+ALTER TABLE database."photovoltaic_data_Approvals" RENAME COLUMN "Standard_Title" TO "Statement_Standard_Title";
+
+ALTER TABLE database."photovoltaic_data_Approvals" RENAME COLUMN "Standard_Standardizers" TO "Statement_Standard_Standardizers";
+
+ALTER TABLE database."photovoltaic_data_Approvals" RENAME COLUMN "Standard_Section" TO "Statement_Standard_Section";
+
+ALTER TABLE database."photovoltaic_data_Approvals" RENAME COLUMN "Standard_Numeration_Suffix" TO "Statement_Standard_Numeration_Suffix";
+
+ALTER TABLE database."photovoltaic_data_Approvals" RENAME COLUMN "Standard_Numeration_Prefix" TO "Statement_Standard_Numeration_Prefix";
+
+ALTER TABLE database."photovoltaic_data_Approvals" RENAME COLUMN "Standard_Numeration_MainNumber" TO "Statement_Standard_Numeration_MainNumber";
+
+ALTER TABLE database."photovoltaic_data_Approvals" RENAME COLUMN "Standard_Locator" TO "Statement_Standard_Locator";
+
+ALTER TABLE database."photovoltaic_data_Approvals" RENAME COLUMN "Standard_Abstract" TO "Statement_Standard_Abstract";
+
+ALTER TABLE database."photovoltaic_data_Approvals" RENAME COLUMN "Publication_WebAddress" TO "Statement_Publication_WebAddress";
+
+ALTER TABLE database."photovoltaic_data_Approvals" RENAME COLUMN "Publication_Urn" TO "Statement_Publication_Urn";
+
+ALTER TABLE database."photovoltaic_data_Approvals" RENAME COLUMN "Publication_Title" TO "Statement_Publication_Title";
+
+ALTER TABLE database."photovoltaic_data_Approvals" RENAME COLUMN "Publication_Section" TO "Statement_Publication_Section";
+
+ALTER TABLE database."photovoltaic_data_Approvals" RENAME COLUMN "Publication_Doi" TO "Statement_Publication_Doi";
+
+ALTER TABLE database."photovoltaic_data_Approvals" RENAME COLUMN "Publication_Authors" TO "Statement_Publication_Authors";
+
+ALTER TABLE database."photovoltaic_data_Approvals" RENAME COLUMN "Publication_ArXiv" TO "Statement_Publication_ArXiv";
+
+ALTER TABLE database."photovoltaic_data_Approvals" RENAME COLUMN "Publication_Abstract" TO "Statement_Publication_Abstract";
+
+ALTER TABLE database."optical_data_Approvals" RENAME COLUMN "Standard_Year" TO "Statement_Standard_Year";
+
+ALTER TABLE database."optical_data_Approvals" RENAME COLUMN "Standard_Title" TO "Statement_Standard_Title";
+
+ALTER TABLE database."optical_data_Approvals" RENAME COLUMN "Standard_Standardizers" TO "Statement_Standard_Standardizers";
+
+ALTER TABLE database."optical_data_Approvals" RENAME COLUMN "Standard_Section" TO "Statement_Standard_Section";
+
+ALTER TABLE database."optical_data_Approvals" RENAME COLUMN "Standard_Numeration_Suffix" TO "Statement_Standard_Numeration_Suffix";
+
+ALTER TABLE database."optical_data_Approvals" RENAME COLUMN "Standard_Numeration_Prefix" TO "Statement_Standard_Numeration_Prefix";
+
+ALTER TABLE database."optical_data_Approvals" RENAME COLUMN "Standard_Numeration_MainNumber" TO "Statement_Standard_Numeration_MainNumber";
+
+ALTER TABLE database."optical_data_Approvals" RENAME COLUMN "Standard_Locator" TO "Statement_Standard_Locator";
+
+ALTER TABLE database."optical_data_Approvals" RENAME COLUMN "Standard_Abstract" TO "Statement_Standard_Abstract";
+
+ALTER TABLE database."optical_data_Approvals" RENAME COLUMN "Publication_WebAddress" TO "Statement_Publication_WebAddress";
+
+ALTER TABLE database."optical_data_Approvals" RENAME COLUMN "Publication_Urn" TO "Statement_Publication_Urn";
+
+ALTER TABLE database."optical_data_Approvals" RENAME COLUMN "Publication_Title" TO "Statement_Publication_Title";
+
+ALTER TABLE database."optical_data_Approvals" RENAME COLUMN "Publication_Section" TO "Statement_Publication_Section";
+
+ALTER TABLE database."optical_data_Approvals" RENAME COLUMN "Publication_Doi" TO "Statement_Publication_Doi";
+
+ALTER TABLE database."optical_data_Approvals" RENAME COLUMN "Publication_Authors" TO "Statement_Publication_Authors";
+
+ALTER TABLE database."optical_data_Approvals" RENAME COLUMN "Publication_ArXiv" TO "Statement_Publication_ArXiv";
+
+ALTER TABLE database."optical_data_Approvals" RENAME COLUMN "Publication_Abstract" TO "Statement_Publication_Abstract";
+
+ALTER TABLE database."hygrothermal_data_Approvals" RENAME COLUMN "Standard_Year" TO "Statement_Standard_Year";
+
+ALTER TABLE database."hygrothermal_data_Approvals" RENAME COLUMN "Standard_Title" TO "Statement_Standard_Title";
+
+ALTER TABLE database."hygrothermal_data_Approvals" RENAME COLUMN "Standard_Standardizers" TO "Statement_Standard_Standardizers";
+
+ALTER TABLE database."hygrothermal_data_Approvals" RENAME COLUMN "Standard_Section" TO "Statement_Standard_Section";
+
+ALTER TABLE database."hygrothermal_data_Approvals" RENAME COLUMN "Standard_Numeration_Suffix" TO "Statement_Standard_Numeration_Suffix";
+
+ALTER TABLE database."hygrothermal_data_Approvals" RENAME COLUMN "Standard_Numeration_Prefix" TO "Statement_Standard_Numeration_Prefix";
+
+ALTER TABLE database."hygrothermal_data_Approvals" RENAME COLUMN "Standard_Numeration_MainNumber" TO "Statement_Standard_Numeration_MainNumber";
+
+ALTER TABLE database."hygrothermal_data_Approvals" RENAME COLUMN "Standard_Locator" TO "Statement_Standard_Locator";
+
+ALTER TABLE database."hygrothermal_data_Approvals" RENAME COLUMN "Standard_Abstract" TO "Statement_Standard_Abstract";
+
+ALTER TABLE database."hygrothermal_data_Approvals" RENAME COLUMN "Publication_WebAddress" TO "Statement_Publication_WebAddress";
+
+ALTER TABLE database."hygrothermal_data_Approvals" RENAME COLUMN "Publication_Urn" TO "Statement_Publication_Urn";
+
+ALTER TABLE database."hygrothermal_data_Approvals" RENAME COLUMN "Publication_Title" TO "Statement_Publication_Title";
+
+ALTER TABLE database."hygrothermal_data_Approvals" RENAME COLUMN "Publication_Section" TO "Statement_Publication_Section";
+
+ALTER TABLE database."hygrothermal_data_Approvals" RENAME COLUMN "Publication_Doi" TO "Statement_Publication_Doi";
+
+ALTER TABLE database."hygrothermal_data_Approvals" RENAME COLUMN "Publication_Authors" TO "Statement_Publication_Authors";
+
+ALTER TABLE database."hygrothermal_data_Approvals" RENAME COLUMN "Publication_ArXiv" TO "Statement_Publication_ArXiv";
+
+ALTER TABLE database."hygrothermal_data_Approvals" RENAME COLUMN "Publication_Abstract" TO "Statement_Publication_Abstract";
+
+ALTER TABLE database."geometric_data_Approvals" RENAME COLUMN "Standard_Year" TO "Statement_Standard_Year";
+
+ALTER TABLE database."geometric_data_Approvals" RENAME COLUMN "Standard_Title" TO "Statement_Standard_Title";
+
+ALTER TABLE database."geometric_data_Approvals" RENAME COLUMN "Standard_Standardizers" TO "Statement_Standard_Standardizers";
+
+ALTER TABLE database."geometric_data_Approvals" RENAME COLUMN "Standard_Section" TO "Statement_Standard_Section";
+
+ALTER TABLE database."geometric_data_Approvals" RENAME COLUMN "Standard_Numeration_Suffix" TO "Statement_Standard_Numeration_Suffix";
+
+ALTER TABLE database."geometric_data_Approvals" RENAME COLUMN "Standard_Numeration_Prefix" TO "Statement_Standard_Numeration_Prefix";
+
+ALTER TABLE database."geometric_data_Approvals" RENAME COLUMN "Standard_Numeration_MainNumber" TO "Statement_Standard_Numeration_MainNumber";
+
+ALTER TABLE database."geometric_data_Approvals" RENAME COLUMN "Standard_Locator" TO "Statement_Standard_Locator";
+
+ALTER TABLE database."geometric_data_Approvals" RENAME COLUMN "Standard_Abstract" TO "Statement_Standard_Abstract";
+
+ALTER TABLE database."geometric_data_Approvals" RENAME COLUMN "Publication_WebAddress" TO "Statement_Publication_WebAddress";
+
+ALTER TABLE database."geometric_data_Approvals" RENAME COLUMN "Publication_Urn" TO "Statement_Publication_Urn";
+
+ALTER TABLE database."geometric_data_Approvals" RENAME COLUMN "Publication_Title" TO "Statement_Publication_Title";
+
+ALTER TABLE database."geometric_data_Approvals" RENAME COLUMN "Publication_Section" TO "Statement_Publication_Section";
+
+ALTER TABLE database."geometric_data_Approvals" RENAME COLUMN "Publication_Doi" TO "Statement_Publication_Doi";
+
+ALTER TABLE database."geometric_data_Approvals" RENAME COLUMN "Publication_Authors" TO "Statement_Publication_Authors";
+
+ALTER TABLE database."geometric_data_Approvals" RENAME COLUMN "Publication_ArXiv" TO "Statement_Publication_ArXiv";
+
+ALTER TABLE database."geometric_data_Approvals" RENAME COLUMN "Publication_Abstract" TO "Statement_Publication_Abstract";
+
+ALTER TABLE database."calorimetric_data_Approvals" RENAME COLUMN "Standard_Year" TO "Statement_Standard_Year";
+
+ALTER TABLE database."calorimetric_data_Approvals" RENAME COLUMN "Standard_Title" TO "Statement_Standard_Title";
+
+ALTER TABLE database."calorimetric_data_Approvals" RENAME COLUMN "Standard_Standardizers" TO "Statement_Standard_Standardizers";
+
+ALTER TABLE database."calorimetric_data_Approvals" RENAME COLUMN "Standard_Section" TO "Statement_Standard_Section";
+
+ALTER TABLE database."calorimetric_data_Approvals" RENAME COLUMN "Standard_Numeration_Suffix" TO "Statement_Standard_Numeration_Suffix";
+
+ALTER TABLE database."calorimetric_data_Approvals" RENAME COLUMN "Standard_Numeration_Prefix" TO "Statement_Standard_Numeration_Prefix";
+
+ALTER TABLE database."calorimetric_data_Approvals" RENAME COLUMN "Standard_Numeration_MainNumber" TO "Statement_Standard_Numeration_MainNumber";
+
+ALTER TABLE database."calorimetric_data_Approvals" RENAME COLUMN "Standard_Locator" TO "Statement_Standard_Locator";
+
+ALTER TABLE database."calorimetric_data_Approvals" RENAME COLUMN "Standard_Abstract" TO "Statement_Standard_Abstract";
+
+ALTER TABLE database."calorimetric_data_Approvals" RENAME COLUMN "Publication_WebAddress" TO "Statement_Publication_WebAddress";
+
+ALTER TABLE database."calorimetric_data_Approvals" RENAME COLUMN "Publication_Urn" TO "Statement_Publication_Urn";
+
+ALTER TABLE database."calorimetric_data_Approvals" RENAME COLUMN "Publication_Title" TO "Statement_Publication_Title";
+
+ALTER TABLE database."calorimetric_data_Approvals" RENAME COLUMN "Publication_Section" TO "Statement_Publication_Section";
+
+ALTER TABLE database."calorimetric_data_Approvals" RENAME COLUMN "Publication_Doi" TO "Statement_Publication_Doi";
+
+ALTER TABLE database."calorimetric_data_Approvals" RENAME COLUMN "Publication_Authors" TO "Statement_Publication_Authors";
+
+ALTER TABLE database."calorimetric_data_Approvals" RENAME COLUMN "Publication_ArXiv" TO "Statement_Publication_ArXiv";
+
+ALTER TABLE database."calorimetric_data_Approvals" RENAME COLUMN "Publication_Abstract" TO "Statement_Publication_Abstract";
+
+ALTER TABLE database."photovoltaic_data_Approvals" ADD "Statement_Exists" boolean NOT NULL DEFAULT TRUE;
+
+ALTER TABLE database."optical_data_Approvals" ADD "Statement_Exists" boolean NOT NULL DEFAULT TRUE;
+
+ALTER TABLE database."hygrothermal_data_Approvals" ADD "Statement_Exists" boolean NOT NULL DEFAULT TRUE;
+
+ALTER TABLE database."geometric_data_Approvals" ADD "Statement_Exists" boolean NOT NULL DEFAULT TRUE;
+
+ALTER TABLE database."calorimetric_data_Approvals" ADD "Statement_Exists" boolean NOT NULL DEFAULT TRUE;
+
+INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20250530151605_ModelReferenceProperly', '9.0.5');
+
+COMMIT;
+
---
+++ backend/src/Migrations/rollback_from_20250331141152_AddAccessRights_to_20250530151605_ModelReferenceProperly.sql
@@ -0,0 +1,228 @@
+﻿START TRANSACTION;
+ALTER TABLE database."photovoltaic_data_Approvals" DROP COLUMN "Statement_Exists";
+
+ALTER TABLE database."optical_data_Approvals" DROP COLUMN "Statement_Exists";
+
+ALTER TABLE database."hygrothermal_data_Approvals" DROP COLUMN "Statement_Exists";
+
+ALTER TABLE database."geometric_data_Approvals" DROP COLUMN "Statement_Exists";
+
+ALTER TABLE database."calorimetric_data_Approvals" DROP COLUMN "Statement_Exists";
+
+ALTER TABLE database."photovoltaic_data_Approvals" RENAME COLUMN "Statement_Standard_Year" TO "Standard_Year";
+
+ALTER TABLE database."photovoltaic_data_Approvals" RENAME COLUMN "Statement_Standard_Title" TO "Standard_Title";
+
+ALTER TABLE database."photovoltaic_data_Approvals" RENAME COLUMN "Statement_Standard_Standardizers" TO "Standard_Standardizers";
+
+ALTER TABLE database."photovoltaic_data_Approvals" RENAME COLUMN "Statement_Standard_Section" TO "Standard_Section";
+
+ALTER TABLE database."photovoltaic_data_Approvals" RENAME COLUMN "Statement_Standard_Numeration_Suffix" TO "Standard_Numeration_Suffix";
+
+ALTER TABLE database."photovoltaic_data_Approvals" RENAME COLUMN "Statement_Standard_Numeration_Prefix" TO "Standard_Numeration_Prefix";
+
+ALTER TABLE database."photovoltaic_data_Approvals" RENAME COLUMN "Statement_Standard_Numeration_MainNumber" TO "Standard_Numeration_MainNumber";
+
+ALTER TABLE database."photovoltaic_data_Approvals" RENAME COLUMN "Statement_Standard_Locator" TO "Standard_Locator";
+
+ALTER TABLE database."photovoltaic_data_Approvals" RENAME COLUMN "Statement_Standard_Abstract" TO "Standard_Abstract";
+
+ALTER TABLE database."photovoltaic_data_Approvals" RENAME COLUMN "Statement_Publication_WebAddress" TO "Publication_WebAddress";
+
+ALTER TABLE database."photovoltaic_data_Approvals" RENAME COLUMN "Statement_Publication_Urn" TO "Publication_Urn";
+
+ALTER TABLE database."photovoltaic_data_Approvals" RENAME COLUMN "Statement_Publication_Title" TO "Publication_Title";
+
+ALTER TABLE database."photovoltaic_data_Approvals" RENAME COLUMN "Statement_Publication_Section" TO "Publication_Section";
+
+ALTER TABLE database."photovoltaic_data_Approvals" RENAME COLUMN "Statement_Publication_Doi" TO "Publication_Doi";
+
+ALTER TABLE database."photovoltaic_data_Approvals" RENAME COLUMN "Statement_Publication_Authors" TO "Publication_Authors";
+
+ALTER TABLE database."photovoltaic_data_Approvals" RENAME COLUMN "Statement_Publication_ArXiv" TO "Publication_ArXiv";
+
+ALTER TABLE database."photovoltaic_data_Approvals" RENAME COLUMN "Statement_Publication_Abstract" TO "Publication_Abstract";
+
+ALTER TABLE database."optical_data_Approvals" RENAME COLUMN "Statement_Standard_Year" TO "Standard_Year";
+
+ALTER TABLE database."optical_data_Approvals" RENAME COLUMN "Statement_Standard_Title" TO "Standard_Title";
+
+ALTER TABLE database."optical_data_Approvals" RENAME COLUMN "Statement_Standard_Standardizers" TO "Standard_Standardizers";
+
+ALTER TABLE database."optical_data_Approvals" RENAME COLUMN "Statement_Standard_Section" TO "Standard_Section";
+
+ALTER TABLE database."optical_data_Approvals" RENAME COLUMN "Statement_Standard_Numeration_Suffix" TO "Standard_Numeration_Suffix";
+
+ALTER TABLE database."optical_data_Approvals" RENAME COLUMN "Statement_Standard_Numeration_Prefix" TO "Standard_Numeration_Prefix";
+
+ALTER TABLE database."optical_data_Approvals" RENAME COLUMN "Statement_Standard_Numeration_MainNumber" TO "Standard_Numeration_MainNumber";
+
+ALTER TABLE database."optical_data_Approvals" RENAME COLUMN "Statement_Standard_Locator" TO "Standard_Locator";
+
+ALTER TABLE database."optical_data_Approvals" RENAME COLUMN "Statement_Standard_Abstract" TO "Standard_Abstract";
+
+ALTER TABLE database."optical_data_Approvals" RENAME COLUMN "Statement_Publication_WebAddress" TO "Publication_WebAddress";
+
+ALTER TABLE database."optical_data_Approvals" RENAME COLUMN "Statement_Publication_Urn" TO "Publication_Urn";
+
+ALTER TABLE database."optical_data_Approvals" RENAME COLUMN "Statement_Publication_Title" TO "Publication_Title";
+
+ALTER TABLE database."optical_data_Approvals" RENAME COLUMN "Statement_Publication_Section" TO "Publication_Section";
+
+ALTER TABLE database."optical_data_Approvals" RENAME COLUMN "Statement_Publication_Doi" TO "Publication_Doi";
+
+ALTER TABLE database."optical_data_Approvals" RENAME COLUMN "Statement_Publication_Authors" TO "Publication_Authors";
+
+ALTER TABLE database."optical_data_Approvals" RENAME COLUMN "Statement_Publication_ArXiv" TO "Publication_ArXiv";
+
+ALTER TABLE database."optical_data_Approvals" RENAME COLUMN "Statement_Publication_Abstract" TO "Publication_Abstract";
+
+ALTER TABLE database."hygrothermal_data_Approvals" RENAME COLUMN "Statement_Standard_Year" TO "Standard_Year";
+
+ALTER TABLE database."hygrothermal_data_Approvals" RENAME COLUMN "Statement_Standard_Title" TO "Standard_Title";
+
+ALTER TABLE database."hygrothermal_data_Approvals" RENAME COLUMN "Statement_Standard_Standardizers" TO "Standard_Standardizers";
+
+ALTER TABLE database."hygrothermal_data_Approvals" RENAME COLUMN "Statement_Standard_Section" TO "Standard_Section";
+
+ALTER TABLE database."hygrothermal_data_Approvals" RENAME COLUMN "Statement_Standard_Numeration_Suffix" TO "Standard_Numeration_Suffix";
+
+ALTER TABLE database."hygrothermal_data_Approvals" RENAME COLUMN "Statement_Standard_Numeration_Prefix" TO "Standard_Numeration_Prefix";
+
+ALTER TABLE database."hygrothermal_data_Approvals" RENAME COLUMN "Statement_Standard_Numeration_MainNumber" TO "Standard_Numeration_MainNumber";
+
+ALTER TABLE database."hygrothermal_data_Approvals" RENAME COLUMN "Statement_Standard_Locator" TO "Standard_Locator";
+
+ALTER TABLE database."hygrothermal_data_Approvals" RENAME COLUMN "Statement_Standard_Abstract" TO "Standard_Abstract";
+
+ALTER TABLE database."hygrothermal_data_Approvals" RENAME COLUMN "Statement_Publication_WebAddress" TO "Publication_WebAddress";
+
+ALTER TABLE database."hygrothermal_data_Approvals" RENAME COLUMN "Statement_Publication_Urn" TO "Publication_Urn";
+
+ALTER TABLE database."hygrothermal_data_Approvals" RENAME COLUMN "Statement_Publication_Title" TO "Publication_Title";
+
+ALTER TABLE database."hygrothermal_data_Approvals" RENAME COLUMN "Statement_Publication_Section" TO "Publication_Section";
+
+ALTER TABLE database."hygrothermal_data_Approvals" RENAME COLUMN "Statement_Publication_Doi" TO "Publication_Doi";
+
+ALTER TABLE database."hygrothermal_data_Approvals" RENAME COLUMN "Statement_Publication_Authors" TO "Publication_Authors";
+
+ALTER TABLE database."hygrothermal_data_Approvals" RENAME COLUMN "Statement_Publication_ArXiv" TO "Publication_ArXiv";
+
+ALTER TABLE database."hygrothermal_data_Approvals" RENAME COLUMN "Statement_Publication_Abstract" TO "Publication_Abstract";
+
+ALTER TABLE database."geometric_data_Approvals" RENAME COLUMN "Statement_Standard_Year" TO "Standard_Year";
+
+ALTER TABLE database."geometric_data_Approvals" RENAME COLUMN "Statement_Standard_Title" TO "Standard_Title";
+
+ALTER TABLE database."geometric_data_Approvals" RENAME COLUMN "Statement_Standard_Standardizers" TO "Standard_Standardizers";
+
+ALTER TABLE database."geometric_data_Approvals" RENAME COLUMN "Statement_Standard_Section" TO "Standard_Section";
+
+ALTER TABLE database."geometric_data_Approvals" RENAME COLUMN "Statement_Standard_Numeration_Suffix" TO "Standard_Numeration_Suffix";
+
+ALTER TABLE database."geometric_data_Approvals" RENAME COLUMN "Statement_Standard_Numeration_Prefix" TO "Standard_Numeration_Prefix";
+
+ALTER TABLE database."geometric_data_Approvals" RENAME COLUMN "Statement_Standard_Numeration_MainNumber" TO "Standard_Numeration_MainNumber";
+
+ALTER TABLE database."geometric_data_Approvals" RENAME COLUMN "Statement_Standard_Locator" TO "Standard_Locator";
+
+ALTER TABLE database."geometric_data_Approvals" RENAME COLUMN "Statement_Standard_Abstract" TO "Standard_Abstract";
+
+ALTER TABLE database."geometric_data_Approvals" RENAME COLUMN "Statement_Publication_WebAddress" TO "Publication_WebAddress";
+
+ALTER TABLE database."geometric_data_Approvals" RENAME COLUMN "Statement_Publication_Urn" TO "Publication_Urn";
+
+ALTER TABLE database."geometric_data_Approvals" RENAME COLUMN "Statement_Publication_Title" TO "Publication_Title";
+
+ALTER TABLE database."geometric_data_Approvals" RENAME COLUMN "Statement_Publication_Section" TO "Publication_Section";
+
+ALTER TABLE database."geometric_data_Approvals" RENAME COLUMN "Statement_Publication_Doi" TO "Publication_Doi";
+
+ALTER TABLE database."geometric_data_Approvals" RENAME COLUMN "Statement_Publication_Authors" TO "Publication_Authors";
+
+ALTER TABLE database."geometric_data_Approvals" RENAME COLUMN "Statement_Publication_ArXiv" TO "Publication_ArXiv";
+
+ALTER TABLE database."geometric_data_Approvals" RENAME COLUMN "Statement_Publication_Abstract" TO "Publication_Abstract";
+
+ALTER TABLE database."calorimetric_data_Approvals" RENAME COLUMN "Statement_Standard_Year" TO "Standard_Year";
+
+ALTER TABLE database."calorimetric_data_Approvals" RENAME COLUMN "Statement_Standard_Title" TO "Standard_Title";
+
+ALTER TABLE database."calorimetric_data_Approvals" RENAME COLUMN "Statement_Standard_Standardizers" TO "Standard_Standardizers";
+
+ALTER TABLE database."calorimetric_data_Approvals" RENAME COLUMN "Statement_Standard_Section" TO "Standard_Section";
+
+ALTER TABLE database."calorimetric_data_Approvals" RENAME COLUMN "Statement_Standard_Numeration_Suffix" TO "Standard_Numeration_Suffix";
+
+ALTER TABLE database."calorimetric_data_Approvals" RENAME COLUMN "Statement_Standard_Numeration_Prefix" TO "Standard_Numeration_Prefix";
+
+ALTER TABLE database."calorimetric_data_Approvals" RENAME COLUMN "Statement_Standard_Numeration_MainNumber" TO "Standard_Numeration_MainNumber";
+
+ALTER TABLE database."calorimetric_data_Approvals" RENAME COLUMN "Statement_Standard_Locator" TO "Standard_Locator";
+
+ALTER TABLE database."calorimetric_data_Approvals" RENAME COLUMN "Statement_Standard_Abstract" TO "Standard_Abstract";
+
+ALTER TABLE database."calorimetric_data_Approvals" RENAME COLUMN "Statement_Publication_WebAddress" TO "Publication_WebAddress";
+
+ALTER TABLE database."calorimetric_data_Approvals" RENAME COLUMN "Statement_Publication_Urn" TO "Publication_Urn";
+
+ALTER TABLE database."calorimetric_data_Approvals" RENAME COLUMN "Statement_Publication_Title" TO "Publication_Title";
+
+ALTER TABLE database."calorimetric_data_Approvals" RENAME COLUMN "Statement_Publication_Section" TO "Publication_Section";
+
+ALTER TABLE database."calorimetric_data_Approvals" RENAME COLUMN "Statement_Publication_Doi" TO "Publication_Doi";
+
+ALTER TABLE database."calorimetric_data_Approvals" RENAME COLUMN "Statement_Publication_Authors" TO "Publication_Authors";
+
+ALTER TABLE database."calorimetric_data_Approvals" RENAME COLUMN "Statement_Publication_ArXiv" TO "Publication_ArXiv";
+
+ALTER TABLE database."calorimetric_data_Approvals" RENAME COLUMN "Statement_Publication_Abstract" TO "Publication_Abstract";
+
+DELETE FROM "__EFMigrationsHistory"
+WHERE "MigrationId" = '20250530151605_ModelReferenceProperly';
+
+ALTER TABLE database.institution_access_rights DROP CONSTRAINT "PK_institution_access_rights";
+
+ALTER TABLE database.institution_access_rights RENAME TO access_rights;
+
+ALTER TABLE database.access_rights ADD CONSTRAINT "PK_access_rights" PRIMARY KEY ("Id");
+
+DELETE FROM "__EFMigrationsHistory"
+WHERE "MigrationId" = '20250527150325_MoveInstitutionAccessRights';
+
+ALTER TABLE database.photovoltaic_data DROP COLUMN "DataAccessRights_AllowedApplications";
+
+ALTER TABLE database.photovoltaic_data DROP COLUMN "DataAccessRights_AllowedInstitutions";
+
+ALTER TABLE database.photovoltaic_data DROP COLUMN "DataAccessRights_AllowedUserAndQuantity";
+
+ALTER TABLE database.optical_data DROP COLUMN "DataAccessRights_AllowedApplications";
+
+ALTER TABLE database.optical_data DROP COLUMN "DataAccessRights_AllowedInstitutions";
+
+ALTER TABLE database.optical_data DROP COLUMN "DataAccessRights_AllowedUserAndQuantity";
+
+ALTER TABLE database.hygrothermal_data DROP COLUMN "DataAccessRights_AllowedApplications";
+
+ALTER TABLE database.hygrothermal_data DROP COLUMN "DataAccessRights_AllowedInstitutions";
+
+ALTER TABLE database.hygrothermal_data DROP COLUMN "DataAccessRights_AllowedUserAndQuantity";
+
+ALTER TABLE database.geometric_data DROP COLUMN "DataAccessRights_AllowedApplications";
+
+ALTER TABLE database.geometric_data DROP COLUMN "DataAccessRights_AllowedInstitutions";
+
+ALTER TABLE database.geometric_data DROP COLUMN "DataAccessRights_AllowedUserAndQuantity";
+
+ALTER TABLE database.calorimetric_data DROP COLUMN "DataAccessRights_AllowedApplications";
+
+ALTER TABLE database.calorimetric_data DROP COLUMN "DataAccessRights_AllowedInstitutions";
+
+ALTER TABLE database.calorimetric_data DROP COLUMN "DataAccessRights_AllowedUserAndQuantity";
+
+DELETE FROM "__EFMigrationsHistory"
+WHERE "MigrationId" = '20250527144412_AddDataAccessRights';
+
+COMMIT;
+

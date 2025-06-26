15917303d6d6ed6cede1cd54ea8919b1cfdc90bb
christoph-maurer <christoph.maurer@ise.fraunhofer.de>
2025-06-26 16:55:01 +0200

    Create example `weightingSpectrum`

--- backend/test/Play.cs
+++ backend/test/Play.cs
@@ -14,7 +14,7 @@ public sealed class Play
     {
         Console.WriteLine("Do play!");
 
-        // Create example myDataPoints
+        // Create example spectralDataPoints
         var direction = new Direction(8);
         var wavelengths0 = new Wavelengths(450);
         var incidence0 = new Incidence(wavelengths0, direction);
@@ -31,12 +31,16 @@ public sealed class Play
         var dataPoint0 = new DataPoint(incidence0, emergence0, results0);
         var dataPoint1 = new DataPoint(incidence1, emergence1, results1);
         var dataPoint2 = new DataPoint(incidence2, emergence2, results2);
-        IReadOnlyList<DataPoint> myDataPoints = new List<DataPoint> { dataPoint0, dataPoint1, dataPoint2 };
-        Console.WriteLine(myDataPoints);
+        IReadOnlyList<DataPoint> spectralDataPoints = new List<DataPoint> { dataPoint0, dataPoint1, dataPoint2 };
+
+        // Create example weightingSpectrum
+        DataPoint dataPoint10 = new DataPoint(new Incidence(new Wavelengths(500), direction), new Emergence(direction), new Results(2));
+        DataPoint dataPoint11 = new DataPoint(new Incidence(new Wavelengths(600), direction), new Emergence(direction), new Results(0.5));
+        IReadOnlyList<DataPoint> weightingSpectrum = new List<DataPoint> { dataPoint10, dataPoint11 };
 
         // Use the SpectralToIntegralMethod
         SpectralToIntegralMethod mySpectralToIntegralMethod = new SpectralToIntegralMethod();
-        List<DataPoint> integralDataPoints = mySpectralToIntegralMethod.Calculate(myDataPoints, myDataPoints);
+        List<DataPoint> integralDataPoints = mySpectralToIntegralMethod.Calculate(spectralDataPoints, weightingSpectrum);
         Console.WriteLine(integralDataPoints);
         foreach (DataPoint integralDataPoint in integralDataPoints)
         {

3e2b4cf154075dc2f8e63be3cec3e243ef9f79f8
Simon Wacker <simon.wacker@ise.faunhofer.de>
2025-08-28 20:05:01 +0200

    Clean-up signing service a bit

--- backend/src/Services/SigningService.cs
+++ backend/src/Services/SigningService.cs
@@ -13,14 +13,14 @@ public sealed class SigningService(
 {
     public async Task Initialize()
     {
-        var (success, output, diagnostics) = await ExecuteGnuCommand(
+        var (success, output, diagnostics) = await ExecuteCommand(
             $"gpg --batch --passphrase '{appSettings.GnupgPrivateKeyPassphrase}' --import './gpg-keys/{appSettings.GnupgPrivateKeyFilename}'"
         );
     }
 
     public async Task<string> SignData(string data)
     {
-        var (success, signature, diagnostics) = await ExecuteGnuCommand(
+        var (success, signature, diagnostics) = await ExecuteCommand(
             $"gpg --pinentry-mode loopback --batch --passphrase {appSettings.GnupgPrivateKeyPassphrase} --detach-sig --armor --local-user {await ExtractFingerprint()}",
             data
         );
@@ -34,7 +34,7 @@ public sealed class SigningService(
 
     public async Task<string> ExtractFingerprint()
     {
-        var (success, fingerprint, diagnostics) = await ExecuteGnuCommand("gpg --list-secret-keys --with-colons --keyid-format=long | awk -F: '$1==\"fpr\" {printf $10; exit}'");
+        var (success, fingerprint, diagnostics) = await ExecuteCommand("gpg --list-secret-keys --with-colons --keyid-format=long | awk -F: '$1==\"fpr\" {printf $10; exit}'");
         if (!success || fingerprint.IsNullOrWhiteSpace())
         {
             throw new InvalidOperationException($"Failed to extract fingerprint with standard output {fingerprint} and diagnostic information {diagnostics}");
@@ -43,7 +43,7 @@ public sealed class SigningService(
         return fingerprint;
     }
 
-    private async Task<(bool Success, string Output, string Diagnostics)> ExecuteGnuCommand(
+    private async Task<(bool Success, string Output, string Diagnostics)> ExecuteCommand(
         string command,
         string? input = null
     )
@@ -56,16 +56,17 @@ public sealed class SigningService(
                 FileName = "bash",
                 Arguments = $"-c \"{escapedCommand}\"",
                 ErrorDialog = false,
+                UseShellExecute = false,
                 CreateNoWindow = true,
                 RedirectStandardInput = true,
                 RedirectStandardOutput = true,
                 RedirectStandardError = true,
             }
-        );
-        ArgumentNullException.ThrowIfNull(process);
+        ) ?? throw new InvalidOperationException($"The process for command '${command}' with the input '${input}' failed to start.");
         if (input is not null)
         {
-            await process.StandardInput.WriteLineAsync(input);            
+            await process.StandardInput.WriteLineAsync(input);
+            await process.StandardInput.FlushAsync();
         }
         process.StandardInput.Close();
         var output = process.StandardOutput.ReadToEnd();

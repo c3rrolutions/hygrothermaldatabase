30909cc3efaefa8dca44e6669124a2216ecbb215
Simon Wacker <simon.wacker@ise.faunhofer.de>
2025-09-05 14:51:04 +0200

    Add SQL migration and rollback scripts

---
+++ backend/src/Migrations/migrate_from_20250731163636_MakeInstitutionIdOfAccessRightsUnique_to_20250902122837_RenameResponsePropertyOfApprovalsToMessage.sql
@@ -0,0 +1,49 @@
+﻿START TRANSACTION;
+ALTER TABLE database."photovoltaic_data_Approvals" ADD "Variables" jsonb NOT NULL DEFAULT ('{}');
+
+ALTER TABLE database.photovoltaic_data ADD "Approval_Variables" jsonb DEFAULT ('{}');
+
+ALTER TABLE database."optical_data_Approvals" ADD "Variables" jsonb NOT NULL DEFAULT ('{}');
+
+ALTER TABLE database.optical_data ADD "Approval_Variables" jsonb DEFAULT ('{}');
+
+ALTER TABLE database."hygrothermal_data_Approvals" ADD "Variables" jsonb NOT NULL DEFAULT ('{}');
+
+ALTER TABLE database.hygrothermal_data ADD "Approval_Variables" jsonb DEFAULT ('{}');
+
+ALTER TABLE database."geometric_data_Approvals" ADD "Variables" jsonb NOT NULL DEFAULT ('{}');
+
+ALTER TABLE database.geometric_data ADD "Approval_Variables" jsonb DEFAULT ('{}');
+
+ALTER TABLE database."calorimetric_data_Approvals" ADD "Variables" jsonb NOT NULL DEFAULT ('{}');
+
+ALTER TABLE database.calorimetric_data ADD "Approval_Variables" jsonb DEFAULT ('{}');
+
+INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20250812171810_AddQueryVariablesToApprovals', '9.0.8');
+
+ALTER TABLE database."photovoltaic_data_Approvals" RENAME COLUMN "Response" TO "Message";
+
+ALTER TABLE database.photovoltaic_data RENAME COLUMN "Approval_Response" TO "Approval_Message";
+
+ALTER TABLE database."optical_data_Approvals" RENAME COLUMN "Response" TO "Message";
+
+ALTER TABLE database.optical_data RENAME COLUMN "Approval_Response" TO "Approval_Message";
+
+ALTER TABLE database."hygrothermal_data_Approvals" RENAME COLUMN "Response" TO "Message";
+
+ALTER TABLE database.hygrothermal_data RENAME COLUMN "Approval_Response" TO "Approval_Message";
+
+ALTER TABLE database."geometric_data_Approvals" RENAME COLUMN "Response" TO "Message";
+
+ALTER TABLE database.geometric_data RENAME COLUMN "Approval_Response" TO "Approval_Message";
+
+ALTER TABLE database."calorimetric_data_Approvals" RENAME COLUMN "Response" TO "Message";
+
+ALTER TABLE database.calorimetric_data RENAME COLUMN "Approval_Response" TO "Approval_Message";
+
+INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20250902122837_RenameResponsePropertyOfApprovalsToMessage', '9.0.8');
+
+COMMIT;
+
---
+++ backend/src/Migrations/rollback_from_20250731163636_MakeInstitutionIdOfAccessRightsUnique_to_20250902122837_RenameResponsePropertyOfApprovalsToMessage.sql
@@ -0,0 +1,49 @@
+﻿START TRANSACTION;
+ALTER TABLE database."photovoltaic_data_Approvals" RENAME COLUMN "Message" TO "Response";
+
+ALTER TABLE database.photovoltaic_data RENAME COLUMN "Approval_Message" TO "Approval_Response";
+
+ALTER TABLE database."optical_data_Approvals" RENAME COLUMN "Message" TO "Response";
+
+ALTER TABLE database.optical_data RENAME COLUMN "Approval_Message" TO "Approval_Response";
+
+ALTER TABLE database."hygrothermal_data_Approvals" RENAME COLUMN "Message" TO "Response";
+
+ALTER TABLE database.hygrothermal_data RENAME COLUMN "Approval_Message" TO "Approval_Response";
+
+ALTER TABLE database."geometric_data_Approvals" RENAME COLUMN "Message" TO "Response";
+
+ALTER TABLE database.geometric_data RENAME COLUMN "Approval_Message" TO "Approval_Response";
+
+ALTER TABLE database."calorimetric_data_Approvals" RENAME COLUMN "Message" TO "Response";
+
+ALTER TABLE database.calorimetric_data RENAME COLUMN "Approval_Message" TO "Approval_Response";
+
+DELETE FROM "__EFMigrationsHistory"
+WHERE "MigrationId" = '20250902122837_RenameResponsePropertyOfApprovalsToMessage';
+
+ALTER TABLE database."photovoltaic_data_Approvals" DROP COLUMN "Variables";
+
+ALTER TABLE database.photovoltaic_data DROP COLUMN "Approval_Variables";
+
+ALTER TABLE database."optical_data_Approvals" DROP COLUMN "Variables";
+
+ALTER TABLE database.optical_data DROP COLUMN "Approval_Variables";
+
+ALTER TABLE database."hygrothermal_data_Approvals" DROP COLUMN "Variables";
+
+ALTER TABLE database.hygrothermal_data DROP COLUMN "Approval_Variables";
+
+ALTER TABLE database."geometric_data_Approvals" DROP COLUMN "Variables";
+
+ALTER TABLE database.geometric_data DROP COLUMN "Approval_Variables";
+
+ALTER TABLE database."calorimetric_data_Approvals" DROP COLUMN "Variables";
+
+ALTER TABLE database.calorimetric_data DROP COLUMN "Approval_Variables";
+
+DELETE FROM "__EFMigrationsHistory"
+WHERE "MigrationId" = '20250812171810_AddQueryVariablesToApprovals';
+
+COMMIT;
+

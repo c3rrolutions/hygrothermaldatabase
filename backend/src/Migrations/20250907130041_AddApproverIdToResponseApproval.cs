9529ee56684fe66d04d1b6feeaf47e68d3a6e761
Simon Wacker <simon.wacker@ise.faunhofer.de>
2025-09-07 15:03:13 +0200

    Add migration that adds approver ID to response approvals

---
+++ backend/src/Migrations/20250907130041_AddApproverIdToResponseApproval.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddApproverIdToResponseApproval : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "Approval_ApproverId",
+                schema: "database",
+                table: "photovoltaic_data",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "Approval_ApproverId",
+                schema: "database",
+                table: "optical_data",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "Approval_ApproverId",
+                schema: "database",
+                table: "hygrothermal_data",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "Approval_ApproverId",
+                schema: "database",
+                table: "geometric_data",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "Approval_ApproverId",
+                schema: "database",
+                table: "calorimetric_data",
+                type: "uuid",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Approval_ApproverId",
+                schema: "database",
+                table: "photovoltaic_data");
+
+            migrationBuilder.DropColumn(
+                name: "Approval_ApproverId",
+                schema: "database",
+                table: "optical_data");
+
+            migrationBuilder.DropColumn(
+                name: "Approval_ApproverId",
+                schema: "database",
+                table: "hygrothermal_data");
+
+            migrationBuilder.DropColumn(
+                name: "Approval_ApproverId",
+                schema: "database",
+                table: "geometric_data");
+
+            migrationBuilder.DropColumn(
+                name: "Approval_ApproverId",
+                schema: "database",
+                table: "calorimetric_data");
+        }
+    }
+}

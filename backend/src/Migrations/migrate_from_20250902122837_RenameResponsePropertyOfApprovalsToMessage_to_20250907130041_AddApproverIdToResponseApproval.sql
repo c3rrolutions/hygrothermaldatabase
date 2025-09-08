1f30f340d921d5e864cf2979ff6948d16e90b8aa
Simon Wacker <simon.wacker@ise.faunhofer.de>
2025-09-08 15:50:16 +0200

    Add SQL migration and rollback scripts

---
+++ backend/src/Migrations/migrate_from_20250902122837_RenameResponsePropertyOfApprovalsToMessage_to_20250907130041_AddApproverIdToResponseApproval.sql
@@ -0,0 +1,16 @@
+﻿START TRANSACTION;
+ALTER TABLE database.photovoltaic_data ADD "Approval_ApproverId" uuid;
+
+ALTER TABLE database.optical_data ADD "Approval_ApproverId" uuid;
+
+ALTER TABLE database.hygrothermal_data ADD "Approval_ApproverId" uuid;
+
+ALTER TABLE database.geometric_data ADD "Approval_ApproverId" uuid;
+
+ALTER TABLE database.calorimetric_data ADD "Approval_ApproverId" uuid;
+
+INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20250907130041_AddApproverIdToResponseApproval', '9.0.8');
+
+COMMIT;
+
---
+++ backend/src/Migrations/rollback_from_20250902122837_RenameResponsePropertyOfApprovalsToMessage_to_20250907130041_AddApproverIdToResponseApproval.sql
@@ -0,0 +1,16 @@
+﻿START TRANSACTION;
+ALTER TABLE database.photovoltaic_data DROP COLUMN "Approval_ApproverId";
+
+ALTER TABLE database.optical_data DROP COLUMN "Approval_ApproverId";
+
+ALTER TABLE database.hygrothermal_data DROP COLUMN "Approval_ApproverId";
+
+ALTER TABLE database.geometric_data DROP COLUMN "Approval_ApproverId";
+
+ALTER TABLE database.calorimetric_data DROP COLUMN "Approval_ApproverId";
+
+DELETE FROM "__EFMigrationsHistory"
+WHERE "MigrationId" = '20250907130041_AddApproverIdToResponseApproval';
+
+COMMIT;
+

2197981c4f66984df442a57f750a16f401bb8541
Simon Wacker <simon.wacker@ise.faunhofer.de>
2025-10-07 16:25:19 +0200

    Remove unused environment variables of unused `NextAuth.js` library

--- frontend/environment.d.ts
+++ frontend/environment.d.ts
@@ -3,13 +3,8 @@ declare global {
     interface ProcessEnv {
       NEXT_PUBLIC_DATABASE_URL: string;
       NEXT_PUBLIC_METABASE_URL: string;
-      NEXTAUTH_URL: string;
       NEXT_WEBPACK_USEPOLLING: string;
       NODE_ENV: "test" | "development" | "production";
-      AUTH_CLIENT_ID: string;
-      AUTH_CLIENT_SECRET: string;
-      AUTH_SECRET: string;
-      AUTH_JWT_SECRET: string;
     }
   }
 }

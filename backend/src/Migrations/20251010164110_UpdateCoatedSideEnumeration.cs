ac5cddc7ee5038af22db7b0e906d5e9801b275db
Simon Wacker <simon.wacker@ise.faunhofer.de>
2025-10-10 18:43:08 +0200

    Add migration to update the coated side enumeration

---
+++ backend/src/Migrations/20251010164110_UpdateCoatedSideEnumeration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateCoatedSideEnumeration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:Enum:database.coated_side", "both,neither,non_prime,not_applicable,prime,unknown")
+                .OldAnnotation("Npgsql:Enum:database.coated_side", "back,both,front,neither");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:Enum:database.coated_side", "back,both,front,neither")
+                .OldAnnotation("Npgsql:Enum:database.coated_side", "both,neither,non_prime,not_applicable,prime,unknown");
+        }
+    }
+}

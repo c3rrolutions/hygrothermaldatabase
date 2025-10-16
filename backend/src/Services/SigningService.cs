08ce04d2334104dcda9d470d7e5ecaa42b792760
Simon Wacker <simon.wacker@ise.faunhofer.de>
2025-10-16 17:24:07 +0200

    Make the log output more readable

--- backend/src/Services/SigningService.cs
+++ backend/src/Services/SigningService.cs
@@ -19,12 +19,12 @@ public static partial class SigningServiceLogging
 {
     [LoggerMessage(
         Level = LogLevel.Debug,
-        Message = "GNUPG Fingerprint: {Fingerprint}")]
+        Message = "GnuPG Fingerprint: {Fingerprint}")]
     public static partial void ExtractedFingerprint(this ILogger<SigningService> logger, string fingerprint);
 
     [LoggerMessage(
         Level = LogLevel.Debug,
-        Message = "GNUPG Signature: {Signature}")]
+        Message = "GnuPG Signature: {Signature}")]
     public static partial void CreatedSignature(this ILogger<SigningService> logger, string signature);
 
     [LoggerMessage(
@@ -39,22 +39,22 @@ public static partial class SigningServiceLogging
 
     [LoggerMessage(
         Level = LogLevel.Debug,
-        Message = "GNUPG: Execute Command: {Command}")]
+        Message = "GnuPG: Execute Command: {Command}")]
     public static partial void ExecuteCommand(this ILogger<SigningService> logger, string command);
 
     [LoggerMessage(
         Level = LogLevel.Error,
-        Message = "GNUPG Execute Command: {Error}")]
+        Message = "GnuPG Execute Command: {Error}")]
     public static partial void ExecuteCommandDiagnostics(this ILogger<SigningService> logger, string error);
 
     [LoggerMessage(
         Level = LogLevel.Debug,
-        Message = "GNUPG: Execute Command Output: {Output}")]
+        Message = "GnuPG: Execute Command Output: {Output}")]
     public static partial void ExecuteCommandOutput(this ILogger<SigningService> logger, string output);
 
     [LoggerMessage(
         Level = LogLevel.Debug,
-        Message = "GNUPG: Execute Command Result: {result}")]
+        Message = "GnuPG: Execute Command Result: {result}")]
     public static partial void ExecuteCommandExitCode(this ILogger<SigningService> logger, int result);
 }
 

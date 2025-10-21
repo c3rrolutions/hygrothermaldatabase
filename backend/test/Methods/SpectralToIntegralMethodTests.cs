f3d0adf5bee21c35793153b683f80d685872fe5a
christoph-maurer <christoph.maurer@ise.fraunhofer.de>
2025-10-21 17:56:24 +0200

    Add large-file storage. Start with ComplexTest

--- backend/test/Methods/SpectralToIntegralMethodTests.cs
+++ backend/test/Methods/SpectralToIntegralMethodTests.cs
@@ -7,8 +7,32 @@ namespace Database.Tests.Methods;
 public sealed class SpectralToIntegralMethodTests
 {
     [Test]
-    public void CalculateTest()
+    public void CalculateSimpleTest()
     {
+        var spectralDataPoints = new SpectralToIntegralInput([
+            new SpectralToIntegralData([
+                new DataPoint(new Incidence(new Wavelengths(200)), new Results(1)),
+                new DataPoint(new Incidence(new Wavelengths(784)), new Results(1)),
+                new DataPoint(new Incidence(new Wavelengths(785)), new Results(0)),
+                new DataPoint(new Incidence(new Wavelengths(2600)), new Results(0))
+            ])
+        ]);
+        var method = new SpectralToIntegralMethod();
+        var results = method.Calculate(spectralDataPoints);
+        results.En410Visible.Should().BeApproximately(1.0F, 0.000000000000001F);
+        results.En410Solar.Should().BeApproximately(0.3733675409177318F, 0.00000001F);
+        results.Iso9050Solar.Should().BeApproximately(0.212543149312381F, 0.00000001F);
+    }
+
+    [Test]
+    public void CalculateComplexTest()
+    {
+        using var document = await JsonDocument.ParseAsync(
+    await httpResponseContent.ReadAsStreamAsync(),
+    StrictJsonDocumentOptions
+);
+        var parsedInput = document.RootElement.Deserialize<TInput>(s_jsonSerializerOptions)
+    ?? throw new InvalidOperationException();
         var spectralDataPoints = new SpectralToIntegralInput([
             new SpectralToIntegralData([
                 new DataPoint(new Incidence(new Wavelengths(200)), new Results(1)),
